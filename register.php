87b34ebc4aeacccdb30cddeb5ec64059e04b6672
Willy <benavidesb@unicauca.edu.co>
2018-04-17 03:15:10 -0500

    button add to register.php

--- register.php
+++ register.php
@@ -8,11 +8,15 @@
 		<form id="loginForm" action="register.php" method="POST">
 			<h2>Login to your Account</h2>
 			<p>
+				<label for="loginusername">Username</label>
 				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Bart Simpson" required>
 			</p>
 			<p>
+				<label for="loginPassword">Password</label>
 				<input id="loginPassword" name="loginPassword" type="password" required>
 			</p>
+
+			<button type="submit" name="loginButton">LOG IN</button>
 		</form>
 	</div>
 </body>

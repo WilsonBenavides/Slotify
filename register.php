3d3199832ae63b1c2a2bc66c80b0d83454705fac
willix-win <benavidesb@unicauca.edu.co>
2018-07-11 01:28:19 -0500

    Outputting error messages

--- register.php
+++ register.php
@@ -40,16 +40,20 @@
 			</p>
 
 			<p>
+				<?php echo $account->getError("Your first name must be between 2 and 25 characters"); ?>
 				<label for="firstName">First name</label>
 				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
 			</p>
 
 			<p>
+				<?php echo $account->getError("Your last name must be between 2 and 25 characters"); ?>
 				<label for="lastName">Last name</label>
 				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
 			</p>
 
 			<p>
+				<?php echo $account->getError("Your emails don't match"); ?>
+				<?php echo $account->getError("Email is invalid"); ?>
 				<label for="email">Email</label>
 				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
 			</p>
@@ -60,6 +64,9 @@
 			</p>
 
 			<p>
+				<?php echo $account->getError("Your passwords don't match"); ?>
+				<?php echo $account->getError("Your password can only contain numbers and letters"); ?>
+				<?php echo $account->getError("Your password must be between 5 and 30 characters"); ?>
 				<label for="password">Password</label>
 				<input id="password" name="password" type="password" placeholder="Your password" required>
 			</p>
